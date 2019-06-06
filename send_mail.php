14ca1f1a8198329d503c11d92ae44a31526562d9
Sidharth Mishra <smishra9@buffalo.edu>
2019-06-06 00:55:30 -0400

    complted send mail server side part

--- send_mail.php
+++ send_mail.php
@@ -1,34 +1,24 @@
 <?php
+  //if "email-id" available send email
+  if (isset($_REQUEST['email-id']))  {
 
- require "EmailManager.php";
+    $email = $_REQUEST['email-id'];
+    $subject = "Confirmation code for the evaluation";
 
- $sendTo = $_POST["email"];
- $sendTo = htmlentities($sendTo);
+    //Construct email body to be sent
+    $confirmationCode = "CONFIRMED";
+    $msg = "Dear Student,\r\nThank you for initiating evaluation process.\r\n".
+         "Use confirmation code: ". $confirmationCode .
+         " to begin the evaluation.\r\n P.S. ".
+         "The code will be valid for 15 minutes only.\r\nRegards,\r\nInstructor/TA";
 
- // Construct email body to be sent
- $mail = new Email();
- $subject = "Confirmation code for the evaluation";
- $confirmCode = "CONFIRMED"
+    //send message with code to student
+    mail($email, "$subject", $msg, "From:evaluationsystem@buffalo.edu".'Reply-To: no-reply@buffalo.com');
 
- $msg = "Dear Student,\nThank you for initiating evaluation process.\nUse confirmation code: ". $confirmCode . " to
-   begin the evaluation.\nP.S. The code will be valid for 15 minutes only.\nRegards,\nCourse Instructor/TA";
-
- // set message to send with the code
- $htmlMessage = "
-   Here is your confirmation code:<br/><br/><b>" . $confirmCode . "</b><br/><br/> Use this code to access
-   your evaluation. If it is not used in 15 minutes you will need to request a new one.";
-
- // call the code to send the message
- try {
-   //send mail to student
-   mail($sendTo, "$subject", $msg, "From:evaluationsystem@buffalo.edu".'Reply-To: no-reply@buffalo.com')
-
-   // return success confirmation
-   echo "Your confirmation code is successfully sent to your email";
-   echo "Thank you for contacting us!";
- } catch (Exception $e) {
-   // Any other error that is not a user error
-   echo "Sorry! Something went wrong. Please try again later.";
- }
-
+    print "Thank you for contacting us!\n";
+    print "Your confirmation code is successfully sent to your email: ".$email;
+  }
+  else  {
+    echo "HTML Attribute 'Required' is not set in the HTML input form.";
+  }
 ?>
