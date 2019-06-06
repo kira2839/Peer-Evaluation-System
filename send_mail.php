3db474e2c7491ff1cea3b857ada9394fee92c613
Sidharth Mishra <smishra9@buffalo.edu>
2019-06-06 01:25:26 -0400

    Integration issues resolve

--- send_mail.php
+++ send_mail.php
@@ -7,13 +7,13 @@
 
     //Construct email body to be sent
     $confirmationCode = "CONFIRMED";
-    $msg = "Dear Student,\r\nThank you for initiating evaluation process.\r\n".
+    $msg = "Dear Student,\r\n\r\nThank you for initiating evaluation process.\r\n".
          "Use confirmation code: ". $confirmationCode .
-         " to begin the evaluation.\r\n P.S. ".
-         "The code will be valid for 15 minutes only.\r\nRegards,\r\nInstructor/TA";
+         " to begin the evaluation.\r\nP.S. ".
+         "The code will be valid for 15 minutes only.\r\n\r\nRegards,\r\nInstructor/TA";
 
     //send message with code to student
-    mail($email, "$subject", $msg, "From:evaluationsystem@buffalo.edu".'Reply-To: no-reply@buffalo.com');
+    mail($email, "$subject", $msg, "From:evaluationsystem@buffalo.edu\r\nReply-To: no-reply@buffalo.com");
 
     print "Thank you for contacting us!\n";
     print "Your confirmation code is successfully sent to your email: ".$email;
