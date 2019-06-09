30a3c49689ea17a095bd2ab347fc36ac5a80e628
Sidharth Mishra <sidharthkmishra@gmail.com>
2019-06-09 14:52:10 -0400

    Fixed the UI related bugs -  #62 #63 #64 #67 #68 #69 #70 #65
     #62 #63 #64 #67 #68 #69 #70 #65

--- login_page.js
+++ login_page.js
@@ -1,45 +1,112 @@
-function isValid(){
-var email_id = document.getElementById('email_id').value;
-email_id = email_id.toLowerCase();
+function isValid(inputMailId) {
+    var email_id = inputMailId.value.toLowerCase();
 
-if(email_id.endsWith("@buffalo.edu")){
-if(!(email_id.toLowerCase() === ".@buffalo.edu")){
-if((email_id.match(/\./g) || []).length<3&&(email_id.match(/@/g) || []).length<2&&email_id.split("@")[0].match("^[a-z0-9.]+$")){
-	if(document.getElementById('submit_button').disabled){
-	document.getElementById('submit_button').disabled=false;}
-	mouseOver();
-}
-else{
-	alert("Your Email ID can contain either letters and numbers or letters with optional '.'");
-	if(!document.getElementById('submit_button').disabled){
-	document.getElementById('submit_button').disabled=true;}
-	mouseOut();
-	
-}
-}
-else{
-	alert("Your Email ID cannot be .@buffalo.edu");
-	mouseOut();
-}
-}
-else
-{
-	alert("Kindly make sure that you enter your UB Email ID");
-	if(!document.getElementById('submit_button').disabled){
-	document.getElementById('submit_button').disabled=true;}
-	mouseOut();
-	
-}
+    if (email_id.endsWith("@buffalo.edu")) {
+        if (!(email_id === ".@buffalo.edu")) {
+            if (!((email_id.match(/\./g) || []).length < 3 && (email_id.match(/@/g) || []).length < 2 && email_id.split("@")[0].match("^[a-z0-9.]+$"))) {
+                document.getElementById("email_id").classList.add('ui-state-error');
+                document.getElementById("error_text").innerHTML = "Valid Email ID can contain only letters, numbers and '.'";
+                document.getElementById("error_span").classList.add('ui-icon');
+                document.getElementById("error_span").classList.add('ui-icon-alert');
+                return false;
+            }
+            postToServer();
+            return false;
+        } else {
+            document.getElementById("email_id").classList.add('ui-state-error');
+            document.getElementById("error_text").innerHTML = "Invalid Email Address";
+            document.getElementById("error_span").classList.add('ui-icon');
+            document.getElementById("error_span").classList.add('ui-icon-alert');
+            return false;
+        }
+    } else {
+        document.getElementById("email_id").classList.add('ui-state-error');
+        document.getElementById("error_text").innerHTML = "Kindly make sure that you enter your UB Email Address";
+        document.getElementById("error_span").classList.add('ui-icon');
+        document.getElementById("error_span").classList.add('ui-icon-alert');
+        return false;
+    }
 }
 
-function mouseOver() {
-  document.getElementById("submit_button").style.backgroundColor = "green";
+function postToServer() {
+    $.ajax({
+        type: "POST",
+        url: "send_mail.php",
+        data: {email_id: document.email_form.email_id.value}
+    }).done(function (msg) {
+        document.getElementById("from_server_text").innerHTML = msg;
+        document.getElementById("from_server_span").classList.add('ui-icon');
+        document.getElementById("from_server_span").classList.add('ui-icon-circle-check');
+    });
+};
+
+function isValidMailAndConfirmation(inputMailId, inputConfirmationCode) {
+    var email_id = inputMailId.value.toLowerCase();
+
+    if (email_id.endsWith("@buffalo.edu")) {
+        if (!(email_id === ".@buffalo.edu")) {
+            if (!((email_id.match(/\./g) || []).length < 3 && (email_id.match(/@/g) || []).length < 2 && email_id.split("@")[0].match("^[a-z0-9.]+$"))) {
+                document.getElementById("email_id_tab2").classList.add('ui-state-error');
+                document.getElementById("error_text_tab2").innerHTML = "Valid Email ID can contain only letters, numbers and '.'";
+                document.getElementById("error_span_tab2").classList.add('ui-icon');
+                document.getElementById("error_span_tab2").classList.add('ui-icon-alert');
+                return false;
+            }
+        } else {
+            document.getElementById("email_id_tab2").classList.add('ui-state-error');
+            document.getElementById("error_text_tab2").innerHTML = "Invalid Email Address";
+            document.getElementById("error_span_tab2").classList.add('ui-icon');
+            document.getElementById("error_span_tab2").classList.add('ui-icon-alert');
+            return false;
+        }
+    } else {
+        document.getElementById("email_id_tab2").classList.add('ui-state-error');
+        document.getElementById("error_text_tab2").innerHTML = "Kindly make sure that you enter your UB Email Address";
+        document.getElementById("error_span_tab2").classList.add('ui-icon');
+        document.getElementById("error_span_tab2").classList.add('ui-icon-alert');
+        return false;
+    }
+
+    var confirmation_code = inputConfirmationCode.value;
+    if (confirmation_code.length < 0) {
+        document.getElementById("confirmation_code").classList.add('ui-state-error');
+        document.getElementById("error_confirmation_text").innerHTML = "Invalid confirmation code";
+        document.getElementById("error_confirmation_span").classList.add('ui-icon');
+        document.getElementById("error_confirmation_span").classList.add('ui-icon-alert');
+    }
+
 }
 
-function mouseOut() {
-  document.getElementById("submit_button").style.backgroundColor = "";
+function isValidConfirmation(inputConfirmationCode) {
+    var confirmation_code = inputConfirmationCode.value;
+    if (confirmation_code.length < 0) {
+        document.getElementById("confirmation_code").classList.add('ui-state-error');
+        document.getElementById("error_confirmation_text").innerHTML = "Invalid confirmation code";
+        document.getElementById("error_confirmation_span").classList.add('ui-icon');
+        document.getElementById("error_confirmation_span").classList.add('ui-icon-alert');
+        return false;
+    }
+    return true;
 }
 
-function onSubmit(){
-// document.getElementById('email_id').value = document.getElementById('email_id').value + "@buffalo.edu";
+function clearClass() {
+    document.getElementById("email_id").classList.remove('ui-state-error');
+    document.getElementById("error_text").innerHTML = "";
+    document.getElementById("error_span").classList.remove('ui-icon-alert');
+    document.getElementById("error_span").classList.remove('ui-icon');
+    document.getElementById("from_server_text").innerHTML = "";
+    document.getElementById("from_server_span").classList.remove('ui-icon-circle-check');
+    document.getElementById("from_server_span").classList.remove('ui-icon');
+}
+
+function clearClassTab2() {
+    document.getElementById("email_id_tab2").classList.remove('ui-state-error');
+    document.getElementById("error_text_tab2").innerHTML = "";
+    document.getElementById("error_span_tab2").classList.remove('ui-icon-alert');
+    document.getElementById("error_span_tab2").classList.remove('ui-icon');
+
+    document.getElementById("confirmation_code").classList.remove('ui-state-error');
+    document.getElementById("error_confirmation_text").innerHTML = "";
+    document.getElementById("error_confirmation_span").classList.remove('ui-icon-alert');
+    document.getElementById("error_confirmation_span").classList.remove('ui-icon');
 }
--- send_mail.php
+++ send_mail.php
@@ -1,24 +1,33 @@
 <?php
-  //if "email-id" available send email
-  if (isset($_REQUEST['email-id']))  {
 
-    $email = $_REQUEST['email-id'];
+if (isset($_POST['email_id'])) {
+
+    $email = $_POST['email_id'];
     $subject = "Confirmation code for the evaluation";
 
     //Construct email body to be sent
     $confirmationCode = "CONFIRMED";
-    $msg = "Dear Student,\r\n\r\nThank you for initiating evaluation process.\r\n".
-         "Use confirmation code: ". $confirmationCode .
-         " to begin the evaluation.\r\nP.S. ".
-         "The code will be valid for 15 minutes only.\r\n\r\nRegards,\r\nInstructor/TA";
+    $msg = "Dear Student,\r\n\r\nThank you for initiating evaluation process.\r\n" .
+        "Use confirmation code: " . $confirmationCode .
+        " to begin the evaluation.\r\nP.S. " .
+        "The code will be valid for 15 minutes only.\r\n\r\nRegards,\r\nInstructor/TA";
 
-    //send message with code to student
-    mail($email, "$subject", $msg, "From:evaluationsystem@buffalo.edu\r\nReply-To: no-reply@buffalo.com");
+    try {
+        //send message with code to student
+        ob_start();
+        $return = mail($email, "$subject", $msg, "From:no-reply@buffalo.com\r\nReply-To: no-reply@buffalo.com");
+        ob_end_clean();
 
-    print "Thank you for contacting us!\n";
-    print "Your confirmation code is successfully sent to your email: ".$email;
-  }
-  else  {
+        if ($return == true) {
+            print "Thank you for contacting us!\n";
+            print "The confirmation code is on its way. Check your email: " . $email;
+        } else {
+            print "Thank you for contacting us!\n";
+            print "The confirmation code is on its way. Try again if you don't receive shortly.";
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+} else {
     echo "HTML Attribute 'Required' is not set in the HTML input form.";
-  }
-?>
+}
