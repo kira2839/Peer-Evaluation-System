09127496c6e819b86ffe9725588d1130ec629d2f
Karthik Vikram <karthikvikramkgk@gmail.com>
2019-06-10 18:29:43 -0400

    Code for adding unique confirmation code based on Email ID and current TimeStamp

--- send_mail.php
+++ send_mail.php
@@ -2,11 +2,34 @@
 
 if (isset($_POST['email_id'])) {
 
-    $email = $_POST['email_id'];
+	$email = $_POST['email_id'];
     $subject = "Confirmation code for the evaluation";
 
+    // Encrypt Script Starts Here
+		$output = false;
+		$encrypt_code = "";
+		$encrypt_method = "AES-256-CBC";
+		$secret_key = 'SECRET_KEY';
+		$secret_iv = 'RANDOM_SEED';
+		// hash
+		$key = hash('sha256', $secret_key);
+		$timeinsecs = time();
+		$currentTime = date('Y-m-d @h:i:s A', $timeinsecs);
+		$email = $email . $currentTime;
+		
+		
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+		$output = openssl_encrypt($email, $encrypt_method, $key, 0, $iv);
+		$confirmationCode = base64_encode($output);
+		
+		// $decryptedMessage = openssl_decrypt(base64_decode($confirmationCode), $encrypt_method, $key, 0, $iv);
+		// echo "Encryption Code is :" .$confirmationCode;
+			
+	// Encrypt Script Ends Here
+	
     //Construct email body to be sent
-    $confirmationCode = "CONFIRMED";
+    // $confirmationCode = "CONFIRMED";
     $msg = "Dear Student,\r\n\r\nThank you for initiating evaluation process.\r\n" .
         "Use confirmation code: " . $confirmationCode .
         " to begin the evaluation.\r\nP.S. " .
@@ -24,7 +47,7 @@ if (isset($_POST['email_id'])) {
         } else {
             print "Thank you for contacting us!\n";
             print "The confirmation code is on its way. Try again if you don't receive shortly.";
-        }
+		
     } catch (Exception $e) {
         echo $e->getMessage();
     }
