f3d685af69287e0b8a54f7db3eff00970fd36a14
Karthik Vikram <karthikvikramkgk@gmail.com>
2019-06-10 21:34:00 -0400

    Intendation Fix #71

--- send_mail.php
+++ send_mail.php
@@ -2,31 +2,31 @@
 
 if (isset($_POST['email_id'])) {
 
-	$email = $_POST['email_id'];
+    $email = $_POST['email_id'];
     $subject = "Confirmation code for the evaluation";
 
     // Encrypt Script Starts Here
-	$output = false;
-	$encrypt_code = "";
-	$encrypt_method = "AES-256-CBC";
-	$secret_key = '190oasidjo*123n-dj';
-	$secret_iv = 'RANDOM_SEED';
-	// hash
-	$key = hash('sha256', $secret_key);
-	$timeinsecs = time();
-	$currentTime = date('Y-m-d @h:i:s A', $timeinsecs);
-	$emailprocessed = $email . $currentTime;
+    $output = false;
+    $encrypt_code = "";
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = '190oasidjo*123n-dj';
+    $secret_iv = 'RANDOM_SEED';
+    // hash
+    $key = hash('sha256', $secret_key);
+    $timeinsecs = time();
+    $currentTime = date('Y-m-d @h:i:s A', $timeinsecs);
+    $emailprocessed = $email . $currentTime;
 	
 	
-	// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
-	$iv = substr(hash('sha256', $secret_iv), 0, 16);
-	$output = openssl_encrypt($emailprocessed, $encrypt_method, $key, 0, $iv);
-	$confirmationCode = base64_encode($output);
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    $output = openssl_encrypt($emailprocessed, $encrypt_method, $key, 0, $iv);
+    $confirmationCode = base64_encode($output);
 	
-	// $decryptedMessage = openssl_decrypt(base64_decode($confirmationCode), $encrypt_method, $key, 0, $iv);
-	// echo "Encryption Code is :" .$confirmationCode;
+    // $decryptedMessage = openssl_decrypt(base64_decode($confirmationCode), $encrypt_method, $key, 0, $iv);
+    // echo "Encryption Code is :" .$confirmationCode;
 			
-	// Encrypt Script Ends Here
+    // Encrypt Script Ends Here
 	
     //Construct email body to be sent
     // $confirmationCode = "CONFIRMED";
