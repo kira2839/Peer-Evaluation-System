8c10c882486a447a0cb1bc8456c0eb383e71851d
Sidharth Mishra <sidharthkmishra@gmail.com>
2019-06-20 08:42:25 -0400

    change password_hash as its not supported at php 5.4 #96

--- backend/student_model.php
+++ backend/student_model.php
@@ -44,7 +44,7 @@ class StudentModel
         $sql = "INSERT INTO " . self::TABLE_NAME .
             "(" . self::EMAIL_ADDRESS_COLUMN . self::COMMA . self::CONFIRMATION_COLUMN . ") values (?, ?)";
 
-        $confirmationCode = password_hash($confirmationCode, PASSWORD_DEFAULT);
+        $confirmationCode = hash('sha256', $confirmationCode);
         $stmt = $this->dbConnector->getDBConnection()->prepare($sql);
         $stmt->bind_param('ss', $email, $confirmationCode);
         $return = $stmt->execute();
@@ -62,7 +62,7 @@ class StudentModel
             self::COMMA . self::IS_CODE_USED_COLUMN . self::EQUAL . " 0 WHERE " .
             self::EMAIL_ADDRESS_COLUMN . self::EQUAL . "?";
 
-        $confirmationCode = password_hash($confirmationCode, PASSWORD_DEFAULT);
+        $confirmationCode = hash('sha256', $confirmationCode);
         $stmt = $this->dbConnector->getDBConnection()->prepare($sql);
         $stmt->bind_param('ss', $confirmationCode, $email);
         $stmt->execute();
